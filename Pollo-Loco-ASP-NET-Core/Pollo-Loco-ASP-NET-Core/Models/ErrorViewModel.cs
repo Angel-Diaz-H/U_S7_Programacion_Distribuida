3b034d30ac3de6c2485971f0236973f77bf3e5dc
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-21 20:16:01 -0600

    Inicio de proyecto Pollo Loco Clon

---
+++ Pollo-Loco-ASP-NET-Core/Pollo-Loco-ASP-NET-Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pollo_Loco_ASP_NET_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

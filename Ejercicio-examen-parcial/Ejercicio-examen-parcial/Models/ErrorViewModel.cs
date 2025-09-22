096be0f2a454c0732fc828f3a017ef12a9f5fd74
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-21 20:11:48 -0600

    Agregué .gitignore y limpié archivos temporales de VS

---
+++ Ejercicio-examen-parcial/Ejercicio-examen-parcial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ejercicio_examen_parcial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
